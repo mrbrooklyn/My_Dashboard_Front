c3927e3cac3dc9a3e0ac85e1b951bf29e6c4e258
Brook <book.damrongdech@gmail.com>
2025-08-29 17:20:36 +0700

    feat: add basic snap scroll

---
+++ app/components/banner/contact-banner.vue
@@ -0,0 +1,19 @@
+<template>
+    <div
+        class="w-full h-screen bg-no-repeat bg-center bg-cover snap-start"
+        :style="{ backgroundImage: `url(${BannerUrl})` }"
+    >
+        this is contact banner
+    </div>
+
+</template>
+
+<script setup lang="ts">
+import BannerUrl from '~/assets/desktop/images/banner/banner-03.png'
+
+export interface Props {
+    // title: string
+    // content: string
+}
+defineProps<Props>()
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="h-screen overflow-y-scroll snap-y snap-mandatory">
+    <BannerHomeBanner 
+      title="Lorem ipsum"
+      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim"
+    />
+    <BannerSectionBanner />
+    <BannerContactBanner />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
